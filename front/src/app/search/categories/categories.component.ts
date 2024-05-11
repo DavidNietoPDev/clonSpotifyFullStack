bc019b2d1d8703bfa438c123e7e9fc0302f3cb61
DavidNP <nietopdavid@gmail.com>
2024-05-11 20:18:07 +0200

    funcionalidad página categories id

--- front/src/app/search/categories/categories.component.ts
+++ front/src/app/search/categories/categories.component.ts
@@ -4,6 +4,7 @@ import { TakeIdsService } from '../../services/take-ids.service';
 import { ExtractColorService } from '../../services/extract-color.service';
 import { Category } from '../../models/categry.model';
 import { SearchServiceService } from '../../services/search-service.service';
+import { MusicPlayerService } from '../../services/music-player.service';
 
 
 @Component({
@@ -16,6 +17,9 @@ export class CategoriesComponent implements OnInit {
   categoriesSearch = inject(SearchBarService)
   takeIdAll = inject(TakeIdsService)
   extractColor = inject(ExtractColorService)
+  musicPlayer = inject(MusicPlayerService)
+
+  globalVolume: number = 0.1;
   loading: boolean = false;
   buscar: boolean = false;
   colorDominanteCategory: number [] = [];
@@ -24,9 +28,6 @@ export class CategoriesComponent implements OnInit {
   imageCategory: string = '';
   nameCategory: string = '';
 
-  imageSimilar: string[] = [];
-  artistSimilarList: string[] = [];
-  artistSimilarId: string [] = []; 
 
   topTrackId: string[] = [];
   topTrackUrl: string[] = [];
@@ -44,16 +45,21 @@ export class CategoriesComponent implements OnInit {
   topAlbumArtist: string[] = [];
   topAlbumYear: string[] = [];
   idsAlbum: string[] = [];
+  idsArtistAlbum: string[] = [];
 
 
   idsPlaylist: string[] = [];
-  playList: string[] = [];
+  playListAutor: string[] = [];
   playListImage: string[] = [];
   playListName: string[] = [];
+
   idArtists: string [] = [];
-  idArtist: string = '';
-  imageUrl: string = '';
-  artist: any;
+  nameArtists: string[] = [];
+  imageArtists: string[] = [];
+
+
+
+  
 
 
   ngOnInit(): void {
@@ -67,14 +73,110 @@ export class CategoriesComponent implements OnInit {
 
   buscarCategories() {
     this.loading = true;
+    this.buscar = true;
+
+    this.colorDominanteCategory = [];
+
+    this.idsPlaylist = [];
+    this.playListImage = [];
+    this.playListName = [];
+    this.playListAutor = [];
+
+    this.idArtists = [];
+    this.nameArtists = [];
+    this.imageArtists = [];
+
+    this.topAlbum = [];
+    this.topAlbumImage = [];
+    this.topAlbumArtist = [];
+    this.topAlbumYear = [];
+    this.idsAlbum = [];
+    this.idsArtistAlbum = [];
+
+    this.topTrackArtistTwo = [];
+    this.idArtistTracksTwo = [];
+    this.topTrackArtist = [];
+    this.idArtistTracks = [];
+    this.topTrackId = [];
+    this.topTrackArtistImage = [];
+    this.topTrackUrl = [];
+    this.topTrack = [];
+    this.topTrackDuration = [];
+
     this.categoriesSearch.getCategoryId(this.categoryId).subscribe((response: Category) => {
       this.imageCategory = response.categoryInfo.icons[0].url;
       this.colorDominante(this.imageCategory)
       this.nameCategory = response.categoryInfo.name;
+      for (let playlist of response.searchs.playlists.items) 
+        {
+        this.idsPlaylist.push(playlist.id);
+        this.playListImage.push(playlist.images[0].url);
+        this.playListName.push(playlist.name)
+        this.playListAutor.push(playlist.owner.display_name)
+        }
+      for (let artist of response.searchs.artists.items)
+        {
+          this.idArtists.push(artist.id);
+          this.nameArtists.push(artist.name);
+          if (artist.images && artist.images.length > 0) 
+            {
+              this.imageArtists.push(artist.images[0].url);
+            } else {
+              this.imageArtists.push('../../assets/Artistasinfoto.png');
+            }
+          
+        }
+      for (let album of response.searchs.albums.items)
+        {
+          this.topAlbum.push(album.name) 
+          this.topAlbumImage.push(album.images[0].url);
+          this.topAlbumArtist.push(album.artists[0].name)
+          this.topAlbumYear.push(album.release_date) 
+          this.idsAlbum.push(album.id); 
+          this.idsArtistAlbum.push(album.artists[0].id)
+        }
+      for (let track of response.searchs.tracks.items)
+        {
+          if(track.artists && track.artists.length > 1) 
+            {
+              this.topTrackArtistTwo.push(track.artists[1].name)
+              this.idArtistTracksTwo.push(track.artists[1].id)
+            } else {
+              this.idArtistTracksTwo.push('')
+              this.topTrackArtistTwo.push('')
+            }
+          this.topTrackArtist.push(track.artists[0].name)
+          this.idArtistTracks.push(track.artists[0].id)
+          this.topTrackId.push(track.id)
+          this.topTrackArtistImage.push(track.album.images[0].url)
+          this.topTrackUrl.push(track.preview_url)
+          this.topTrack.push(track.name)
+          this.topTrackDuration.push(track.duration_ms)
 
+        }
+        
     })
     this.loading = false;
   }
+
+
+   //Zona de Reproducción
+   stopMusic() {
+    this.musicPlayer.stopMusic();
+  }
+
+  togglePlayBack(previewUrl: string) {
+    this.musicPlayer.togglePlayBack(previewUrl);
+  }
+
+  setVolume(volume: number) {
+    this.musicPlayer.setVolume(volume);
+  }
+
+  ajustarVolume() {
+    this.musicPlayer.setVolume(this.globalVolume);
+  }
+
 
   async colorDominante(imageUrl: string) {
     this.extractColor.getColorDominante(imageUrl)
